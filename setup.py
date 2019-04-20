e31e14a76ce1c4ed447848cce356bb209e147cb2
Frank Lehner <frank.lehner@online.de>
2019-04-20 17:07:12 +0200

    setup: Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+setup.py
+========
+"""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="machine-learning",
+    version="0.0.1",
+    author="Frank Lehner",
+    author_email="frank-lehner71@t-online.de",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "Cython",
+        "scikit-learn",
+        "tensorflow",
+        "theano",
+        "keras",
+        "h5py",
+        "tables",
+        "pillows",
+        "opencv-python",
+        "gensim",
+        "click",
+    ]
+)
