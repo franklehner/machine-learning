e353dd4fbb600e2e9d9a193e2f0f474cfa32f59e
Frank Lehner <frank-lehner71@t-online.de>
2024-01-03 11:51:08 +0100

    views/prepare_dataset: Add wine dataset

--- libs/machine_learning/views/prepare_dataset.py
+++ libs/machine_learning/views/prepare_dataset.py
@@ -5,6 +5,7 @@ from typing import Optional, Tuple
 import numpy as np
 
 from libs.machine_learning.views.iris import Iris
+from libs.machine_learning.views.wine import Wine
 
 Tensor = np.ndarray
 SplittedData = Tuple[Tensor, Tensor, Tensor, Tensor]
@@ -13,6 +14,10 @@ SplittedData = Tuple[Tensor, Tensor, Tensor, Tensor]
 def get_origin_iris_dataset() -> Iris:
     "get the dataset without restriction"
     return Iris()
+
+def get_origin_wine_dataset() -> Wine:
+    "Get the wine dataset without restriction"
+    return Wine()
 
 
 def get_splitted_dataset(iris: Optional[Iris] = None, count: Optional[int] = None) -> SplittedData:
@@ -25,6 +30,11 @@ def get_splitted_dataset(iris: Optional[Iris] = None, count: Optional[int] = Non
         x_train, x_test, y_train, y_test = iris.split(count=count)
 
     return x_train, x_test, y_train, y_test
+
+def get_splitted_wine_dataset() -> SplittedData:
+    "Get splitted wine dataset"
+    wine = get_origin_wine_dataset()
+    return wine.split()
 
 
 def get_standardized_data(count: Optional[int] = None) -> SplittedData:
@@ -39,3 +49,14 @@ def get_standardized_data(count: Optional[int] = None) -> SplittedData:
     )
 
     return x_train, x_test, y_train, y_test
+
+def get_standardized_wine_data() -> SplittedData:
+    "Get standardized wine dataset with mean 0 and std 1"
+    wine = get_origin_wine_dataset()
+    x_train, x_test, y_train, y_test = wine.split()
+    x_train_std, x_test_std = wine.standardize(
+        x_train=x_train,
+        x_test=x_test,
+    )
+
+    return x_train_std, x_test_std, y_train, y_test
