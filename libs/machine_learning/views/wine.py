dd0f8530ef43caecfc32024de081088a4b2eda38
Frank Lehner <frank-lehner71@t-online.de>
2024-01-12 15:05:50 +0100

    views/wine: Encode class labels

--- libs/machine_learning/views/wine.py
+++ libs/machine_learning/views/wine.py
@@ -5,7 +5,7 @@ from typing import List, Optional, Tuple
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 DEFAULT_FILENAME = "data/wine.csv"
 Tensor = np.ndarray
@@ -29,7 +29,8 @@ class Wine:
         df = pd.read_csv(DEFAULT_FILENAME)
         value_cols = [col for col in df.columns if col != "Klassenbezeichnung"]
         self.data = df[value_cols].values
-        self.targets = np.array(df["Klassenbezeichnung"].values)
+        le = LabelEncoder()
+        self.targets = le.fit_transform(np.array(df["Klassenbezeichnung"].values))
         self.feature_names = value_cols
         self.target_names = df["Klassenbezeichnung"].unique()
 
